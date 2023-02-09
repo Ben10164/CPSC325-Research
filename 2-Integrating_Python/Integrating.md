612f4ac5dd838f7c3c473949bb89c2b8dc749566
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2023-02-09 09:29:28 -0800

    Added python-ly testing :(

---
+++ 2-Integrating_Python/Integrating.md
@@ -0,0 +1,20 @@
+# LilyPond in python?
+
+## ython-ly
+
+https://pypi.org/project/python-ly/
+
+^ so this library was really annoying.  
+i spent about an hour trying to implement it so here are some of the issues i faced while doing so
+
+```py
+from ly import *
+
+# this doesnt work because there is no __all__ in the __init__.py file for python-ly
+```
+
+the documentation was very odd.
+
+I did not enjoy using this module
+
+there was no examples in the documentation, and i found 0 instances of it being used before
---
+++ 2-Integrating_Python/python-ly.py
@@ -0,0 +1,32 @@
+# https://github.com/frescobaldi/python-ly
+# http://python-ly.readthedocs.org
+import ly
+# from ly import words, etreeutil, reformat, rests, rhythm, util, indent, pitch, pkginfo, server, slexer, data, docinfo, document, dom, duration, lex, xml, cli, colorize, cursortools, barcheck, node, music, musicxml
+
+
+import ly.music.read
+import ly.music.event
+import ly.music.items
+import ly.document
+
+
+with open('LilyPond-Python/Clair_de_Lune.ly', 'r') as file:
+    data = file.read()
+
+# print(data)
+doc = ly.document.Document(data)
+
+# for block in doc._blocks:
+#     print(doc.tokens_with_position(block))
+# 
+# for i in range(len(doc)):
+#     print(doc.block(i))
+
+run = ly.document.Runner(doc)
+print(run.token()) # this will print the first token (which is a comment)
+
+cur = ly.document.Cursor(doc, 0, 0) # cursor is at position 0, 0
+
+print(cur.text())
+
+# wow this module is weird
